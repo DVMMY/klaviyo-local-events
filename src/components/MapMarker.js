9b9468a902a051ca6b5b202fb25964b766fcc568
Dammy <damola-adeniyi@hotmail.co.uk>
2025-02-19 00:27:43 +0000

    Update Klaviyo API calls to v3

--- src/components/MapMarker.js
+++ src/components/MapMarker.js
@@ -3,6 +3,9 @@ import { Marker, InfoWindow } from "@react-google-maps/api";
 
 const MapMarker = ({ item, position }) => {
   const [markerHover, setMarkerHover] = useState(false);
+  let last_name = item.attributes.last_name ? ' ' + item.attributes.last_name : '';
+  let markerTitle = item.attributes.first_name || item.attributes.last_name ? item.attributes.first_name + last_name : item.attributes.email;
+  // console.log('item', item);
 
   return (
     <Marker
@@ -14,14 +17,14 @@ const MapMarker = ({ item, position }) => {
         <InfoWindow position={position}>
           <div className='marker-content'>
             <h4 className='title'>
-              {item.$first_name} {item.$last_name}
+              {markerTitle}
             </h4>
-            {item.EventOneName && (
+            {item.attributes.properties.EventOneName && (
               <div className='events'>
                 <h5 className='subtitle'>Events applied:</h5>
                 <p>
-                  {item.EventOneName}
-                  <br /> {item.EventTwoName}
+                  {item.attributes.properties.EventOneName}
+                  <br /> {item.attributes.properties.EventTwoName}
                 </p>
               </div>
             )}
