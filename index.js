60f95af40690d801cf85523f6630d15b0eeb3a6c
Dammy <damola-adeniyi@hotmail.co.uk>
2025-02-19 00:24:21 +0000

    Update Klaviyo API calls to v3

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const axios = require("axios");
 require("dotenv").config();
 
 const app = express();
-
 app.use(cors());
 
 // app.get("/", (req, res) => {
@@ -15,61 +14,66 @@ app.use(cors());
 // RETRIEVE ALL LISTS FROM KLAVIYO ACCOUNT
 app.get("/lists", (req, res) => {
   const privateKey = req.query.privateKey;
-  const options = {
-    method: "GET",
-    url: `https://a.klaviyo.com/api/v2/lists?&api_key=${privateKey}`,
-    headers: { Accept: "application/json" },
-  };
-
-  axios
-    .request(options)
-    .then((response) => {
-      res.json(response.data);
-      console.log("response.data", response.data);
-    })
-    .catch((error) => {
-      console.error("error", error);
-    });
+  
+  async function fetchAllLists() {
+    let allLists = [];
+    let url = 'https://a.klaviyo.com/api/lists';
+    
+    try {
+      while (true) {
+        const response = await axios.get(url, {
+          headers: { 
+            accept: 'application/vnd.api+json',
+            revision: '2025-01-15',
+            Authorization: `Klaviyo-API-Key ${privateKey}`
+           }
+        });
+  
+        allLists.push(...response.data.data);  
+        url = response.data.links && response.data.links.next ? response.data.links.next : null;
+        if (!url) {
+          break;  
+        }
+      }
+    } catch (error) {
+      console.error('Error fetching lists:', error);
+    }
+    res.json(allLists);
+  }
+  fetchAllLists();
 });
 
 // RETRIEVE MEMBERS FROM CHOSEN KLAVIYO LIST
 app.get("/listMembers", (req, res) => {
   const privateKey = req.query.privateKey;
   const listId = req.query.selectedList;
-  const options = {
-    method: "GET",
-    url: `https://a.klaviyo.com/api/v2/group/${listId}/members/all?api_key=${privateKey}`,
-    headers: { Accept: "application/json" },
-  };
 
-  axios
-    .request(options)
-    .then((response) => {
-      res.json(response.data);
-    })
-    .catch((error) => {
-      console.error("error", error);
-    });
-});
-
-// GET PROFILE DATA FROM KLAVIYO
-app.get("/getProfile", (req, res) => {
-  const privateKey = req.query.privateKey;
-  const personId = req.query.personId;
-  const options = {
-    method: "GET",
-    url: `https://a.klaviyo.com/api/v1/person/${personId}?api_key=${privateKey}`,
-    headers: { Accept: "application/json" },
-  };
-
-  axios
-    .request(options)
-    .then((response) => {
-      res.json(response.data);
-    })
-    .catch((error) => {
-      console.error("error", error);
-    });
+  async function fetchAllListMembers() {
+    let allMembers = [];
+    let url = `https://a.klaviyo.com/api/lists/${listId}/profiles`;
+    
+    try {
+      while (true) {
+        const response = await axios.get(url, {
+          headers: { 
+            accept: 'application/vnd.api+json',
+            revision: '2025-01-15',
+            Authorization: `Klaviyo-API-Key ${privateKey}`
+            }
+        });
+  
+        allMembers.push(...response.data.data);  
+        url = response.data.links && response.data.links.next ? response.data.links.next : null;
+        if (!url) {
+          break;  
+        }
+      }
+    } catch (error) {
+      console.error('Error fetching list members:', error);
+    }
+    res.json(allMembers);
+  }
+  fetchAllListMembers();
 });
 
 // GET EVENT DATA FROM TICKETMASTER
@@ -105,14 +109,16 @@ app.put("/updateProfile", (req, res) => {
   const eventTwoImage = req.query.eventTwoImage;
   const eventTwoLink = req.query.eventTwoLink;
 
-  const options = {
-    method: "PUT",
-    url: `https://a.klaviyo.com/api/v1/person/${personId}?EventOneName=${eventOneName}&EventOneDate=${eventOneDate}&EventOneImage=${eventOneImage}&EventOneLink=${eventOneLink}&EventTwoName=${eventTwoName}&EventTwoDate=${eventTwoDate}&EventTwoImage=${eventTwoImage}&EventTwoLink=${eventTwoLink}&api_key=${privateKey}`,
-    headers: { Accept: "application/json" },
-  };
+  let header = { headers: {accept: 'application/vnd.api+json',
+    revision: '2025-01-15',
+    'content-type': 'application/vnd.api+json',
+    Authorization: `Klaviyo-API-Key ${privateKey}`
+  }};
+  let url = `https://a.klaviyo.com/api/profiles/${personId}`;
+  let body = `{"data":{"type":"profile","id":"${personId}","attributes":{"properties":{"EventOneName":"${eventOneName}","EventOneDate":"${eventOneDate}","EventOneImage":"${eventOneImage}","EventOneLink":"${eventOneLink}","EventTwoName":"${eventTwoName}","EventTwoDate":"${eventTwoDate}","EventTwoImage":"${eventTwoImage}","EventTwoLink":"${eventTwoLink}"}}}}`;
 
   axios
-    .request(options)
+    .patch(url, body, header)
     .then((response) => {
       res.json(response.data);
     })
@@ -121,23 +127,35 @@ app.put("/updateProfile", (req, res) => {
     });
 });
 
-// GET EMAIL TEMPLATES FROM KLAVIYO ACCOUNT
 app.get("/getTemplates", (req, res) => {
   const privateKey = req.query.privateKey;
-  const options = {
-    method: "GET",
-    url: `https://a.klaviyo.com/api/v1/email-templates?page=0&count=100&api_key=${privateKey}`,
-    headers: { Accept: "application/json" },
-  };
-
-  axios
-    .request(options)
-    .then((response) => {
-      res.json(response.data);
-    })
-    .catch((error) => {
-      console.error("error", error);
-    });
+  async function fetchAllTemplates() {
+    let allTemplates = [];
+    let url = 'https://a.klaviyo.com/api/templates';
+    
+    try {
+      while (true) {
+        const response = await axios.get(url, {
+          headers: { 
+            accept: 'application/vnd.api+json',
+            revision: '2025-01-15',
+            Authorization: `Klaviyo-API-Key ${privateKey}`
+           }
+        });
+  
+        allTemplates.push(...response.data.data);  
+        url = response.data.links && response.data.links.next ? response.data.links.next : null;
+        if (!url) {
+          break;  
+        }
+      }
+    } catch (error) {
+      console.error('Error fetching lists:', error);
+    }
+    // console.log('allTemplates',allTemplates);
+    res.json(allTemplates);
+  }
+  fetchAllTemplates();
 });
 
 // CREATE NEW KLAVIYO CAMPAIGN
@@ -145,38 +163,91 @@ app.post("/createCampaign", (req, res) => {
   const privateKey = req.query.privateKey;
   const listId = req.query.listId;
   const selectedTemplate = req.query.selectedTemplate;
+  let messageId = ''
 
-  const { URLSearchParams } = require("url");
-  const encodedParams = new URLSearchParams();
+  async function createNewCampaign() {
+    try {
+      let headerCampaign = { headers: {
+        accept: 'application/vnd.api+json',
+        revision: '2025-01-15',
+        'content-type': 'application/vnd.api+json',
+        Authorization: `Klaviyo-API-Key ${privateKey}`
+      }};
+      let urlCampaign = 'https://a.klaviyo.com/api/campaigns';
+      let bodyCampaign = `{
+      "data": {
+        "type": "campaign",
+        "attributes": {
+          "audiences": {
+            "included": [
+              "${listId}"
+            ]
+          },
+          "send_options": {
+            "use_smart_sending": true
+          },
+          "campaign-messages": {
+            "data": [
+              {
+                "type": "campaign-message",
+                "attributes": {
+                  "definition": {
+                    "channel": "email",
+                    "label": "Local Events",
+                    "content": {
+                      "subject": "Local Events"
+                    }
+                  }
+                }
+              }
+            ]
+          },
+          "name": "Local Events"
+        }
+      }
+    }`;
+    
+    const campaignResponse = await axios
+      .post(urlCampaign, bodyCampaign, headerCampaign)
+      .then((response) => {
+        messageId = response.data.data.relationships['campaign-messages'].data[0].id;
+      })
 
-  encodedParams.set("list_id", `${listId}`);
-  encodedParams.set("template_id", `${selectedTemplate}`);
-  encodedParams.set("from_email", "dammy.adeniyi@klaviyo.com");
-  encodedParams.set("from_name", "Local Events");
-  encodedParams.set("subject", "Local Events");
-  encodedParams.set("name", "Local Events");
-  encodedParams.set("use_smart_sending", "true");
-  encodedParams.set("add_google_analytics", "false");
+      let headerMessage = { headers: {
+        accept: 'application/vnd.api+json',
+        revision: '2025-01-15',
+        'content-type': 'application/vnd.api+json',
+        Authorization: `Klaviyo-API-Key ${privateKey}`
+      }};
+      let urlMessage = 'https://a.klaviyo.com/api/campaign-message-assign-template';
+      let bodyMessage = `{
+        "data": {
+          "type": "campaign-message",
+          "relationships": {
+            "template": {
+              "data": {
+                "type": "template",
+                "id": "${selectedTemplate}"
+              }
+            }
+          },
+          "id": "${messageId}"
+        }
+      }`;
 
-  const options = {
-    method: "POST",
-    url: `https://a.klaviyo.com/api/v1/campaigns?api_key=${privateKey}`,
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    data: encodedParams,
-  };
+      const messageResponse = await axios
+        .post(urlMessage, bodyMessage, headerMessage)
+        .then((response) => {
+          res.json(response.data);
+      })
 
-  axios
-    .request(options)
-    .then((response) => {
-      res.json(response.data);
-      // console.log("response", response);
-    })
-    .catch((error) => {
-      console.error("error", error);
-    });
+
+    } catch (err) {
+      console.log('Error creating campaign:', err);
+    }
+  }
+  createNewCampaign()
+  
 });
 
 app.listen(8000, () => {
